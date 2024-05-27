14696583bed6b0a02ee5134f7a64abbb8e7f7ce6
bkapturowski <kaptur2601@gmail.com>
2024-05-27 11:13:50 +0200

    17. Generacja dokumentacji dla klasy FifoRhymer

--- src/main/java/edu/kis/vh/nursery/FifoRhymer.java
+++ src/main/java/edu/kis/vh/nursery/FifoRhymer.java
@@ -1,20 +1,35 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa FifoRhymer rozszerza DefaultCountingOutRhymer, aby zaimplementować
+ * strategię liczenia w stylu FIFO (First In First Out). Osiąga się to poprzez
+ * użycie tymczasowego licznika do przechowywania i pobierania wartości w
+ * kolejności FIFO.
+ */
 public class FifoRhymer extends DefaultCountingOutRhymer {
 
     private DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
+    /**
+     * Nadpisuje metodę countOut, aby zaimplementować zachowanie FIFO. Metoda
+     * używa tymczasowego licznika do odwrócenia kolejności elementów.
+     *
+     * @return pierwszy element, który został wstawiony, lub -1, jeśli licznik jest pusty.
+     */
     @Override
     protected int countOut() {
-        while (!callCheck())
-
+        // Przenieś wszystkie elementy z tego licznika do tymczasowego licznika
+        while (!callCheck()) {
             temp.countIn(super.countOut());
+        }
 
+        // Pobierz pierwszy element, który został wstawiony
         final int ret = temp.countOut();
 
-        while (!temp.callCheck())
-
+        // Przenieś wszystkie elementy z powrotem do tego licznika
+        while (!temp.callCheck()) {
             countIn(temp.countOut());
+        }
 
         return ret;
     }
